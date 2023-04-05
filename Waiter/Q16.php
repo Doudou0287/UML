60e6f66e6780e661b9ea9350d3ede91837bfab5a
Sundus <116579651+Doudou0287@users.noreply.github.com>
2023-04-05 13:47:55 +0200

    add Q16 and Q18

--- Waiter/Q16.php
+++ Waiter/Q16.php
@@ -1,4 +1,8 @@
 <?php
+
+
+class Table {}
+
 class Waiter {
     public function __construct(private array $tables = []) {
     }
@@ -7,7 +11,12 @@ class Waiter {
         if (count($this->tables) >= 4) {
             throw new Exception('A waiter cannot be assigned to more than four tables.');
         }
-        array_push($this->tables, $table);
+        if(!in_array($table, $this->tables, true)){
+            $this->tables[] = $table;
+            return true;
+        }
+        return false;
+        // array_push($this->tables, $table);
     }
 
     public function removeTable(Table $table) {
@@ -19,32 +28,12 @@ class Waiter {
         }
     }
 }
+// Create a waiter and tables
 
-class Table {
-    public $waiter = null;
+$table1 = new Table();
+$table2 = new Table();
+// $table3 = new Table();
+// $table4 = new Table();
 
-    public function __construct(Waiter $waiter) {
-        $waiter->addTable($this);
-        $this->waiter = $waiter;
-    }
-
-    public function unassignWaiter() {
-        if ($this->waiter === null) {
-            throw new Exception('This table is not assigned to a waiter.');
-        }
-        $this->waiter->removeTable($this);
-        $this->waiter = null;
-    }
-}
-
-// Create a waiter
-$waiter = new Waiter();
-
-// Create some tables and assign them to a waiter
-$table1 = new Table($waiter);
-$table2 = new Table($waiter);
-$table3 = new Table($waiter);
-$table4 = new Table($waiter);
-// $table5 = new Table($waiter ); // this will be used to test the exception for too many tables
-
-var_dump($table1);
+$waiter = new Waiter([$table1 ,$table2]);
+var_dump($waiter);
